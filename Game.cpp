aed10d4590ef0562bd2bafe6788d090cba02fd44
Creska <luca.paparo@studio.unibo.it>
2021-01-26 23:42:57 +0100

    Game.cpp first implementation

---
+++ Game.cpp
@@ -0,0 +1,56 @@
+#include "Game.hpp"
+#include "timercpp.h"
+using namespace std;
+
+Game::Game()
+{
+    //constructor is absent because by setting score and level in the start() method, when the game finishes and you reset it, it automatically reset the score and the level
+}
+
+void Game::start()
+{
+    this->level = 1;
+    this->score = 0;
+    //TODO set the default matrix
+    Timer gameTimer = Timer();
+    gameTimer.setInterval([&]() {
+        this->changeState(gameTimer);
+    },
+                          16); //every 1/60th of a second
+}
+
+void Game::changeState(Timer gameTimer)
+{
+    if (this->gameState != GAME_OVER)
+    {
+        //TODO this is the center of the game, here we'll move sprites and stuff
+    }
+    else
+    {
+        gameTimer.stop();
+    }
+}
+
+void Game::addScore(int score)
+{
+    if (this->internal_score + score > 1000)
+    {
+        this->level++;
+        this->internal_score = 0;
+    }
+    this->score += score;
+    this->internal_score += score;
+}
+
+void Game::removeScore(int score)
+{
+    if (this->score - score <= 0)
+    {
+        this->gameState = GAME_OVER;
+    }
+    else
+    {
+        this->score -= score;
+        this->internal_score -= score;
+    }
+}
---
+++ Game.hpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <string>
+#define GAME_ON 0
+#define GAME_OVER 1
+using namespace std;
+class Game
+{
+private:
+    int gameState;                     //0 active, 1 gameOver
+    int level;                         //current level
+    int score;                         //current score
+    int internal_score;                //score that resets to 0 every time the normal score reaches the next thousand unit
+    vector<vector<string>> canvas;     //graphic container
+    void changeState(Timer gameTimer); //changes the game state by moving the enemies and the bonuses
+
+public:
+    Game();
+    void start();
+    vector<vector<string>> getCanvas();
+    void setCanvas(vector<vector<string>> canvas);
+    int setLevel(int level);
+    int getLevel(int level);
+    void addScore(int score);
+    void removeScore(int score);
+};
