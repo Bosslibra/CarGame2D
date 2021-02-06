168ed69d691b58e86e4529977d27413ce2ad9a9a
Creska <luca.paparo@studio.unibo.it>
2021-02-06 18:41:29 +0100

    Input testing, Input files fixed

---
+++ Input.hpp
@@ -0,0 +1,11 @@
+class Input {
+    private:
+        int x_coord;
+        int y_coord;
+
+
+    public: 
+        Input();
+        int getMovementInput();
+        int getMenuInput();
+};
---
+++ InputTest.cpp
@@ -0,0 +1,54 @@
+#include "Input.cpp"
+#include <iostream>
+#include <windows.h>
+
+int main()
+{
+    Input i;
+    bool keepGoing = true;
+
+    do
+    {
+        int coord = i.getMovementInput();
+        if (coord == TOP)
+        {
+            std::cout << "top ";
+        }
+        else if (coord == LEFT)
+        {
+            std::cout << "left ";
+        }
+        else if (coord == RIGHT)
+        {
+            std::cout << "right ";
+        }
+        else if (coord == DOWN)
+        {
+            std::cout << "down ";
+        }
+        else if (coord == TOPLEFT)
+        {
+            std::cout << "topleft ";
+        }
+        else if (coord == TOPRIGHT)
+        {
+            std::cout << "topright ";
+        }
+        else if (coord == DOWNLEFT)
+        {
+            std::cout << "downleft ";
+        }
+        else if (coord == DOWNRIGHT)
+        {
+            std::cout << "downright ";
+        }
+        if (i.getMenuInput() == ENTER)
+        {
+            keepGoing = false;
+        }
+        Sleep(17);
+
+    } while (keepGoing);
+
+    return 0;
+}
