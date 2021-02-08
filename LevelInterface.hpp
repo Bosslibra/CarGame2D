146f40b4427c40a1a347acc6f2be4b18de35869b
Mattia <mattiababbini99@gmail.com>
2021-02-08 19:01:09 +0100

    commenti

--- LevelInterface.hpp
+++ LevelInterface.hpp
@@ -11,13 +11,25 @@ private:
     int level;
     int score;
     std::vector<std::vector<char>> canvas;
-
+    /**
+     * @brief inizializza il canvas (disegna i bordi)
+     *
+     */
     void initCanvas();
 
 public:
     LevelInterface(int height);
-
+    /**
+     * @brief scrive lo score e il livello nel canvas
+     *
+     * @param level
+     * @param score
+     */
     void drawCanva(int level, int score);
-
+    /**
+     * @brief Get the Canvas object
+     *
+     * @return std::vector<std::vector<char>>
+     */
     std::vector<std::vector<char>> getCanvas();
 };
